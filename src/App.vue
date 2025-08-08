2f089752fccc8ce2607d73282c2f273843befb5a
jessica <qwan0148@student.monash.edu>
2025-08-08 22:50:12 +1000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import JSONLab from './components/JSON.vue'
+</script>
+
+<template>
+  <main>
+    <JSONLab />
+  </main>
+</template>
+
+<style scoped>
+main {
+  padding: 20px;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+</style>
